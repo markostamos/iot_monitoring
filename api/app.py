8ad72f951004ab9a436a8898de9475fb28fe27ff
markostamos <gimarkostamos@gmail.com>
2021-12-02 14:34:45 +0200

    mqqt broker and real time webpage updates

--- api/app.py
+++ api/app.py
@@ -42,16 +42,6 @@ def handle_publish(json_str):
     mqtt.publish(data['topic'], data['message'])
 
 
-@socketio.on('subscribe')
-def handle_subscribe(json_str):
-    data = json.loads(json_str)
-    mqtt.subscribe(data['topic'])
-
-
-@socketio.on('unsubscribe_all')
-def handle_unsubscribe_all():
-    mqtt.unsubscribe_all()
-
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
